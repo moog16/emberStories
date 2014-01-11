3f0fb27a8ace838f68362e3532cf6591919b59dd
mgoo <mgoo@switchfly.com>
2014-01-11 11:10:02 -0800

    saving stories

--- js/app.js
+++ js/app.js
@@ -1,5 +1,53 @@
 App = Ember.Application.create();
  
 App.Router.map(function() {
-  // put your routes here
+  this.resource('newstory', {path: 'story/new'});
+});
+
+App.ApplicationAdapter = DS.LSAdapter.extend({
+  namespace: 'stories'
+});
+
+App.Story = DS.Model.extend({
+  url : DS.attr('string'),
+  tags : DS.attr('string'),
+  fullname : DS.attr('string'),
+  title : DS.attr('string'),
+  excerpt : DS.attr('string'),
+  submittedOn : DS.attr('date')
+});
+
+App.IndexRoute = Ember.Route.extend({
+  model : function(){
+    return this.get('store').findAll('story');
+  }
+});
+
+App.IndexController = Ember.ArrayController.extend({
+    sortProperties : ['submittedOn'],
+    sortAscending : true
+});
+
+App.NewstoryController = Ember.ObjectController.extend({
+  actions: {
+    save: function(){
+      var url = $('#url').val();
+      var tags = $('#tags').val();
+      var fullname = $('#fullname').val();
+      var title = $('#title').val();
+      var excerpt = $('#excerpt').val();
+      var submittedOn = new Date();
+      var store = this.get('store');
+      var story = store.createRecord('story',{
+        url: url,
+        tags: tags,
+        fullname: fullname,
+        title: title,
+        excerpt: excerpt,
+        submittedOn: submittedOn
+      });
+      story.save();
+      this.transitionToRoute('index');
+    }
+  }
 });
